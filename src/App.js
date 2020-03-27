ac177e91293c378cb401fd39f5a65cb6f56dd334
Wenxuan He <wenxh15@vt.edu>
2020-03-26 20:35:03 -0500

    add debounce

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { debounce } from "lodash";
 import "./styles.css";
 
 export default class App extends Component {
+  state = {
+    value: "",
+    list: []
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = debounce(this.handleChange, 500);
+  }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => console.log(res.data));
+  }
+
+  handleChange = e => {
+    axios.get("https://jsonplaceholder.typicode.com/users");
+
+    this.setState({
+      value: e.target.value
+    });
+  };
   render() {
+    const { value } = this.state;
+    console.log(value);
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <input
+          placeholder="placeholder"
+          input={value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
 }
+
+// debounce = (func, delay) => {
+//     let inDebounce
+//     return function() {
+//       const context = this
+//       const args = arguments
+//       clearTimeout(inDebounce)
+//       inDebounce = setTimeout(() => func.apply(context, args), delay)
+//     }
+//   }
