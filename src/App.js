4755c04d06b8ee96f9baab79183646c23f51c400
Wenxuan He <wenxh15@vt.edu>
2020-03-29 19:51:56 -0500

    finish debouncing with function

--- src/App.js
+++ src/App.js
@@ -1,52 +1,87 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { debounce } from "lodash";
-import "./styles.css";
 
 export default class App extends Component {
   state = {
     value: "",
-    list: []
+    dropdownList: [],
+    match: []
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = debounce(this.handleChange, 500);
+  debounce(func, delay) {
+    let debounceTimer;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
   }
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then(res => console.log(res.data));
-  }
+
+  handleSearch = this.debounce(() => {
+    const { value } = this.state;
+    console.log(value, "value");
+    if (value.trim().length > 0) {
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+        .then(res => {
+          if (res.data.meals !== null) {
+            res.data.meals.map(item =>
+              this.setState(state => ({
+                dropdownList: [...state.dropdownList, item.strMeal]
+              }))
+            );
+            let match = this.state.dropdownList.filter(
+              item =>
+                item.toLowerCase().indexOf(this.state.value.toLowerCase()) > -1
+            );
+            this.setState({
+              match: [...new Set(match)]
+            });
+          } else {
+            this.setState({
+              match: []
+            });
+          }
+        });
+    } else {
+      this.setState({
+        match: []
+      });
+    }
+  }, 1000);
 
   handleChange = e => {
-    axios.get("https://jsonplaceholder.typicode.com/users");
-
-    this.setState({
-      value: e.target.value
-    });
+    this.setState(
+      {
+        value: e.target.value
+      },
+      () => this.handleSearch()
+    );
   };
+
+  handleClick = item => {
+    this.setState(() => ({
+      value: item,
+      match: []
+    }));
+  };
+
   render() {
     const { value } = this.state;
-    console.log(value);
     return (
       <div className="App">
         <input
           placeholder="placeholder"
-          input={value}
+          value={value}
           onChange={this.handleChange}
         />
+        {this.state.match.map((item, index) => (
+          <p key={index} onClick={() => this.handleClick(item)}>
+            {item}
+          </p>
+        ))}
       </div>
     );
   }
 }
-
-// debounce = (func, delay) => {
-//     let inDebounce
-//     return function() {
-//       const context = this
-//       const args = arguments
-//       clearTimeout(inDebounce)
-//       inDebounce = setTimeout(() => func.apply(context, args), delay)
-//     }
-//   }
